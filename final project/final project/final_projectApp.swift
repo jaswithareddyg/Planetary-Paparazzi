3552d331505bb12e06a7377881530eef8e245458
Jas <jaswithareddy@uchicago.edu>
2023-05-25 20:36:46 -0500

    C1

---
+++ final project/final project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final project
+//
+//  Created by Jaswitha Reddy G on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
