4525c748513c5dc1ca0e3f7f5d5ec0354f9a4a62
Jas <jaswithareddy@uchicago.edu>
2023-05-25 20:25:19 -0500

    C1

---
+++ translator/translator/translatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  translatorApp.swift
+//  translator
+//
+//  Created by Jaswitha Reddy G on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct translatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
